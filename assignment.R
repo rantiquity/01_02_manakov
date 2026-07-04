2a820281afd43548708eee052958ac7423ba977b
firsey74 <147094180+firsey74@users.noreply.github.com>
2026-07-04 21:27:48 +0300

    Update assignment.R

--- assignment.R
+++ assignment.R
@@ -4,20 +4,26 @@ library(tidyverse)
 
 works <- gutenberg_works()
 
-# В каждом пункте используйте оператор pipe, не сохраняйте промежуточные результаты!
-
 # (1) Отберите ряды, в которых gutenberg_author_id равен 65 или 410;
 # после этого выберите два столбца: author, title
-my_data <- works |> 
-  # ваш код здесь
+my_data <- works |>
+  filter(gutenberg_author_id == 65 | gutenberg_author_id == 410) |>
+  # либо:
+  # filter(gutenberg_author_id %in% c(65, 410)) |> 
+  select(author, title)
 
 # (2) Используйте функцию separate(), чтобы разделить 
 # столбец с именем и фамилией на два новых: author, name. 
 # Удалите столбец name
-my_data2 <- my_data |>
-  # ваш код здесь
+  my_data2 <- my_data |>
+    separate(author, into = c("author", "name"), sep = ", ") |> 
+    select(-name)
+    # либо (без создания дополнительного столбца для удаления):
+    # separate(author, c("A", NA), sep = ", ")
 
 # (3) Используйте group_by() и summarise(), чтобы узнать,
 # сколько произведений Шекспира и Марлоу хранится в библиотеке Gutenberg
-my_data3 <- my_data2 |>
-  # ваш код здесь
+  my_data3 <- my_data2 |>
+    group_by(author) |> 
+    summarise(number_of_works = n()) |> 
+    arrange(-number_of_works)
